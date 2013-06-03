1240b8b207c18353b204ddea87d11ae813d8a9b7
Riaz Gaffoor <silverriaz@gmail.com>
2013-06-03 15:19:30 -0400

    Commit

--- Pachondi/urls.py
+++ Pachondi/urls.py
@@ -3,15 +3,17 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
-urlpatterns = patterns('',
-                       (r'^groups/$','groups.views.list_groups'),
-                       (r'^groups/new$','groups.views.new'),  # to create a group
-                       (r'^groups/add/$','groups.views.add'),  # post after create
-                       (r'^groups/(?P<group_id>\d+)','groups.views.show'),
-                       (r'^groups/edit/(?P<group_id>\d+)','groups.views.edit'), # to choose a group to edit
-                       (r'^groups/update/(?P<group_id>\d+)','groups.views.update'), # to handle after form is posted for edit
-                       (r'^groups/delete/(?P<group_id>\d+)','groups.views.delete'),
+# [:index, :show, :new, :create, :edit, :update, :destroy] 
+urlpatterns = patterns('groups.views',
+                       (r'^groups/$','index'), # show all group
+                       (r'^groups/new$','new'),  # to create a group
+                       (r'^groups/create/$','create'),  # post after create
+                       (r'^groups/(?P<group_id>\d+)','show'),
+                       (r'^groups/edit/(?P<group_id>\d+)','edit'), # to edit a group
+                       (r'^groups/update/(?P<group_id>\d+)','update'), # post after edit
+                       (r'^groups/delete/(?P<group_id>\d+)','delete'), # to delete a group
+                       (r'^groups/destroy/(?P<group_id>\d+)','destroy'), # to post after delete
+                       #(r'^groups*','index'), # catch all, defaults to index
     # Examples:
     # url(r'^$', 'Pachondi.views.home', name='home'),
     # url(r'^Pachondi/', include('Pachondi.foo.urls')),
--- groups/views.py
+++ groups/views.py
@@ -1,37 +1,45 @@
 # Create your views here.
 from groups.models import Group
 # from django.template import Context, loader
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect
+#from django.http import HttpResponse
 
-def list_groups(request, message=""):
+# GET /groups
+def index(request, message=""):
     group_list = Group.objects.all()
     return render_to_response(
         'groups/list.html',
         {'group_list':group_list,'message':message}
     )
 
-def new(request):
-    return render_to_response(
-        'groups/form.html',
-        {
-         'action':'add',
-         'button':'Create'
-        }
-    )
-
-def show(request, group_id):
+# GET /groups/:id
+def show(request, group_id, message=""):
     group = Group.objects.get(id=group_id)
     return render_to_response(
                'groups/form.html',                
                {
                 'action':'',
                 'button':'',
-                'Group':group
+                'Group':group,
+                'message':message
                 }                   
            )
-   
-def add(request):
+
+# GET /groups/new
+def new(request):
+    return render_to_response(
+        'groups/form.html',
+        {
+         'action':'create',
+         'button':'Create'
+        }
+    )
+
+# POST /groups
+def create(request):
+    if request.method != "POST":
+        return redirect('groups.views.new')
+    
     group_name = request.POST["group_name"]
     group_description = request.POST["group_description"]
     group = Group(
@@ -39,8 +47,9 @@ def add(request):
                  group_description = group_description      
             )
     group.save()
-    return list_groups(request,"Group Created")
+    return index(request,"Group Created")
 
+#GET /groups/:id/edit
 def edit(request,group_id):
     group = Group.objects.get(id=group_id)
     return render_to_response(
@@ -51,21 +60,103 @@ def edit(request,group_id):
                 'Group':group
                 }                   
            )
- 
+
+#PUT /groups/:id
 def update(request,group_id):
     group = Group.objects.get(id=group_id)
     group.group_name = request.POST["group_name"]
     group.group_description = request.POST["group_description"]
     group.save()
-    return list_groups(request,message="Group " + group.group_name + " Updated.")   
+    return index(request,message="Group " + group.group_name + " Updated.")   
     #html = "<html><body>Testing</body></html>"
     #return HttpResponse(html)
 
+#GET /groups/:id/delete
 def delete(request,group_id):
+    group = Group.objects.get(id=group_id)
+    return render_to_response(
+               'groups/form.html',                
+               {
+                'action':'destroy/' + group_id,
+                'button':'Delete',
+                'Group':group
+                }                   
+           )
+
+#DELETE /groups/:id
+def destroy(request,group_id):
+
+    if request.method != "POST":
+        return redirect('groups.views.index')
+
+    group_name = request.POST["group_name"]
     Group.objects.get(id=group_id).delete()
-    return list_groups(request,message="Group Deleted.")
+    return index(request,message="Group " + group_name + " Deleted.")
 
 #def groups_home(request):
     #html = "<html><body>Testing</body></html>"
     #return HttpResponse(html)
     
+"""
+# GET /groups
+  def index
+    @groups = Group.all
+  end
+  
+  
+  # GET /groups/:id
+  def show
+    #@groups = Group.where("user_id = ?",current_user.id)
+    @group = Group.find(params[:id])
+  end
+
+  # GET /groups/new
+  def new
+    @group = Group.new
+  end
+  
+  # POST /groups
+  def create
+    params[:group].merge!("user_id" => @current_user.id)
+    @group = Group.new(params[:group])
+    if @group.save
+      flash[:notice] = "Group created! #{params} #{params[:group]}"
+      redirect_back_or_default groups_path
+    else
+      render :action => :new
+    end
+  end
+  
+  #GET /groups/:id/edit
+  def edit
+    @group = Group.find(params[:id])
+  end  
+  
+  #PUT /groups/:id
+  def update
+    @group = Group.find(params[:id])
+    
+    if @group.update_attributes(params[:group])
+      flash[:notice] = "Group was successfully updated #{params}"
+      respond_with @group, :location => groups_path
+    end
+    # rails 2 way
+    #respond_to do |format|
+      #if @group.update_attributes(params[:group])
+        #format.html { redirect_to @group, notice:'Group was successfully updated'}
+      #else
+        #format.html { render action:"edit", notice:'Group was unsuccessfully updated'}
+      #end
+    #end 
+  
+  end
+  
+  #DELETE /groups/:id
+  def destroy
+    #Can only deactivate if the group was created by the owner
+    #should only deactivate and not delete the group.  
+    Group.find(params[:id]).destroy
+    flash[:notice] = "Group Deleted!"
+    redirect_back_or_default groups_path
+  end
+  """
